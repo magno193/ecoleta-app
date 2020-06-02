2ef8e7e1e31e85bda2e99b3752ea9f1c05fb3ebd
Alexandre Magno de Lima Ferreira <alexmlf93@gmail.com>
2020-06-02 17:01:21 -0300

    Seed e configuração no server para exibição de arquivos estáticos.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+// Servir arquivos estáticos
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
