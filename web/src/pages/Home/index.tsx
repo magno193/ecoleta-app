69d47311cebc4aba92224ac6ce39e14a3be39bcb
Alexandre Magno de Lima Ferreira <alexmlf93@gmail.com>
2020-06-05 03:47:11 -0300

    Contrução de html do formulário

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -17,7 +17,7 @@ const Home = () => {
                     <h1>Seu marketplace de coleta de resíduos.</h1>
                     <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
 
-                    <Link to="/cadastro">
+                    <Link to="/create-point">
                         <span>
                             <FiLogIn/>
                         </span>
