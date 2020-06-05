1f26078f35b713cfd3b1d089e57294df3e2072ba
Alexandre Magno de Lima Ferreira <alexmlf93@gmail.com>
2020-06-05 03:13:04 -0300

    Configuração de navegação com Link

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { FiLogIn } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+
 import './styles.css'
 
 import logo from '../../assets/logo.svg';
@@ -15,12 +17,12 @@ const Home = () => {
                     <h1>Seu marketplace de coleta de resíduos.</h1>
                     <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
 
-                    <a href="/cadastro">
+                    <Link to="/cadastro">
                         <span>
                             <FiLogIn/>
                         </span>
                         <strong>Cadstre um ponto de coleta</strong>
-                    </a>
+                    </Link >
                 </main>
             </div>
         </div>
