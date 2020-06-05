85b5636c5e9b87cabbb14e6642e67d728dd458eb
Alexandre Magno de Lima Ferreira <alexmlf93@gmail.com>
2020-06-05 00:29:32 -0300

    Inicio do desenvolvimento react

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import './App.css';
-
-import Header from './Header';
-
 function App() {
   return (
-    <div>
-      <Header title="Ecoleta"/>
-      <Header title="Titulo 2"/>
-
-      <h1>Conteúdo</h1>
-    </div>
+    <div>Initial template</div>
   );
 }
 
