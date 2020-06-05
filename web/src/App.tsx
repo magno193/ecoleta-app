26f5d31e8af3d1cccb108dbf6d1a17e88446fbad
Alexandre Magno de Lima Ferreira <alexmlf93@gmail.com>
2020-06-05 00:13:42 -0300

    Componentes e propriedades

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import Header from './Header';
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header title="Ecoleta"/>
+      <Header title="Titulo 2"/>
+
+      <h1>Conteúdo</h1>
+    </div>
   );
 }
 
