5697b29cd845636f3105358cf4479ee03f9aba4e
Alexandre Magno de Lima Ferreira <alexmlf93@gmail.com>
2020-06-05 21:26:25 -0300

    Mobile inicio

---
+++ mobile/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+
+import Home from './src/pages/Home';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+      <Home />
+    </>
+  );
+};
